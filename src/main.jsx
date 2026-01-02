7305a17ce40ab45ea353b7ef9180074d9b85ab3c
陳宗葆 <blueskycm@gmail.com>
2026-01-02 19:45:44 +0800

    Week2 fix: 修正 SCSS 引入路徑並新增深色模式支援

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,8 @@ import { createRoot } from 'react-dom/client'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 
-import './index.css'
+import './assets/all.scss'
+// import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
